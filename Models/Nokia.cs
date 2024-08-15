5019cbbd04a4e85a31770dcf294c7bd577e75c52
oguiaquino <guiiaquino92@gmail.com>
2024-08-14 22:32:24 -0300

    Project Completed

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria)
+         : base(numero, modelo, imei, memoria)
+        {
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo '{nomeApp}' no Nokia...");
+        }
     }
 }
